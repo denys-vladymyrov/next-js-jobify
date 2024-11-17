ec531604f6b9e63db59a1db0726a3c85e4a10f04
Denys Vladymyrov <link_amd@yahoo.com>
2024-11-17 22:41:10 +0200

    added Prisma and React query

--- app/(dashboard)/add-job/page.tsx
+++ app/(dashboard)/add-job/page.tsx
@@ -1,8 +1,18 @@
 import CreateJobForm from "@/components/CreateJobForm";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from '@tanstack/react-query';
 
 function AddJobPage() {
-  return <>
-    <CreateJobForm />
-  </>
+  const queryClient = new QueryClient();
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <CreateJobForm />
+    </HydrationBoundary>
+  );
 }
+
 export default AddJobPage;

8bb82b7ce5ea901650e941065a4a2d894091e7a8
Denys Vladymyrov <link_amd@yahoo.com>
2024-11-23 20:25:56 +0200

    added pagination

--- utils/actions.ts
+++ utils/actions.ts
@@ -43,7 +43,7 @@ export async function createJobAction(values: CreateAndEditJobType): Promise<Job
   }
 }
 
-export async function getAllJobsAction({search, jobStatus, page = 1, limit = 10}: GetAllJobsActionTypes): Promise<{
+export async function getAllJobsAction({search, jobStatus, page = 1, limit = 10,}: GetAllJobsActionTypes): Promise<{
   jobs: JobType[];
   count: number;
   page: number;
@@ -73,22 +73,30 @@ export async function getAllJobsAction({search, jobStatus, page = 1, limit = 10}
         ],
       };
     }
-
     if (jobStatus && jobStatus !== 'all') {
       whereClause = {
         ...whereClause,
         status: jobStatus,
       };
     }
+    const skip = (page - 1) * limit;
 
     const jobs: JobType[] = await prisma.job.findMany({
       where: whereClause,
+      skip,
+      take: limit,
       orderBy: {
         createdAt: 'desc',
       },
     });
 
-    return { jobs, count: 0, page: 1, totalPages: 0 };
+    const count: number = await prisma.job.count({
+      where: whereClause,
+    });
+
+    const totalPages = Math.ceil(count / limit);
+
+    return { jobs, count, page, totalPages };
   } catch (error) {
     console.error(error);
     return { jobs: [], count: 0, page: 1, totalPages: 0 };
